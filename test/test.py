2cd04eef9544dc346f781326f0372ca2b28cb1af
Milenko12AX7 <jofuentes21@alumnos.utalca.cl>
2026-04-14 03:56:22 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -7,7 +7,8 @@ from cocotb.triggers import ClockCycles
 
 
 @cocotb.test()
-async def test_project(dut):cocotb.pass_test()
+async def test_project(dut):
+    cocotb.pass_test()
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
@@ -38,3 +39,4 @@ async def test_project(dut):cocotb.pass_test()
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+

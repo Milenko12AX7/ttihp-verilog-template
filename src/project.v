84e3a288248a2c46af0d6934ea6abe1e29281ba0
Milenko12AX7 <jofuentes21@alumnos.utalca.cl>
2026-04-14 14:17:54 -0400

    Rename module from tt_um_Milenko12AX7_ciclo_lavadora_3bit to tt_um_Milenko12AX7_ciclo_lavadora_4bit

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_Milenko12AX7_ciclo_lavadora_3bit (
+module tt_um_Milenko12AX7_ciclo_lavadora_4bit (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path

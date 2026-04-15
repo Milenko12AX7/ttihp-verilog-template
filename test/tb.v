e4d944a24f5a24214dc62596cb65027a72fe7361
Milenko12AX7 <jofuentes21@alumnos.utalca.cl>
2026-04-14 23:23:12 -0400

    Instantiate DUT for tt_um_Milenko12AX7 module

--- test/tb.v
+++ test/tb.v
@@ -24,7 +24,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_Milenko12AX7_ciclo_lavadora_4bit (
+  tt_um_Milenko12AX7_ciclo_lavadora_4bit dut (
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
